8f17e8f901fef9fc6c85d7552ca3de28b2e6c721
lexpanov <panov1552@gmail.com>
2017-12-15 07:35:12 +0300

    pre-merge

--- config.py
+++
@@ -1,4 +0,0 @@
-DEBUG = True
-SECRET_KEY = 'secret'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///posts.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
--- models.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-class Post(db.Model):
-    __tablename__ = 'posts'
-    id        = db.Column(db.Integer, primary_key = True)
-    date      = db.Column(db.String)
-    author    = db.Column(db.String)
-    subject   = db.Column(db.String)
-    body      = db.Column(db.String)
