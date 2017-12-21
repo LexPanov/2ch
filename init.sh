5810a21e20e9354f3812cf7320978be55301d5ed
LexPanov <panov1552@gmail.com>
2017-12-21 18:19:26 +0300

    missing /n

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@ sudo apt-get install python3 python3-pip
 
 sudo pip3 install -r requirements.txt
 
-python3 -c "import os; print ('\nSECRET_KEY = ' + str(repr(os.urandom(24))));\n" >> app/settings.py
+python3 -c "import os; print ('\nSECRET_KEY = ' + str(repr(os.urandom(24))));" >> app/settings.py
